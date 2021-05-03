17ad79f5a207cc6538cab61ff72186b2879de241
crocimi <mcroci@gmail.com>
2021-05-03 19:30:59 +0200

    Initial Project Add

---
+++ src/test/java/ch/crocimi/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.crocimi.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
